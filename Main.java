41bd5e120823cc958d3c4949f03ce97204e527cf
LordMilton <dufour.bradley@gmail.com>
2016-04-19 12:19:18 -0400

    Completed

--- Main.java
+++ Main.java
@@ -1,10 +1,24 @@
-//TODO: <name>
-//TODO: <date>
+//TODO: Bradley/Thor
+//TODO: 
 //Practice 2013 - 01: Gradabase
+import java.util.Scanner;
 
-public class  {
-  public static void main(String[] args) {
-
-    System.exit(0);
-  }
+public class Main{
+   public static void main(String[] args)throws Exception {
+      Scanner scan = new Scanner(new java.io.File("in-out\\sample.in"));
+      //Scanner scan = new Scanner(System.in);
+      int n = scan.nextInt();
+      for(int i = 0; i < n; i++)
+      {
+         System.out.println("Case "+ (i+1) +":");
+         int m = scan.nextInt();
+         for(int j = 0; j < m; j++)
+         {
+            int num = scan.nextInt();
+            if(num<6)
+               System.out.println(num+1);
+         }
+      }
+      System.exit(0);
+   }
 }
