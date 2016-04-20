b91e3031902df6c3be5bfb5133ddee8c30f835d6
Bradley Dufour <dufour.bradley@gmail.com>
2016-04-20 08:25:48 -0400

    Changed Scanner

--- Main.java
+++ Main.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 
 public class Main{
    public static void main(String[] args)throws Exception {
-      Scanner scan = new Scanner(new java.io.File("in-out\\sample.in"));
-      //Scanner scan = new Scanner(System.in);
+      //Scanner scan = new Scanner(new java.io.File("in-out\\sample.in"));
+      Scanner scan = new Scanner(System.in);
       int n = scan.nextInt();
       for(int i = 0; i < n; i++)
       {
